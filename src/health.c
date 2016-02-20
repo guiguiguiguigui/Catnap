2f5fbbba397ad7b0d02cbf68e4e86dc8c8a5d1e2
Vivien <vchen3@wellesley.edu>
2016-02-20 16:55:09 -0500

    Adding text

--- src/health.c
+++ src/health.c
@@ -2,21 +2,8 @@
 static Window *s_main_window;
 
 static void main_window_load(Window *window){
-     // Get information about the Window
-    Layer *window_layer = window_get_root_layer(window);
-    GRect bounds = layer_get_bounds(window_layer);
 
-    // Create the TextLayer with specific bounds
-    s_time_layer = text_layer_create(
-        GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-    // Improve the layout to be more like a watchface
-    text_layer_set_text(s_time_layer, "ella");
-
-    // Add it as a child layer to the Window's root layer
-    layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
-
 static void main_window_unload(Window *window){
 
 }
