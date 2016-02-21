e3f6fdc17ec3d2b6d61fa1fb176cb363b9a89f42
valz14 <vzhao@wellesley.edu>
2016-02-20 19:36:48 -0500

    Update main_screen.c
    there are some things that definitely need to be changed, since i was working on the cloud -- they're marked with !!!

--- main_screen.c
+++ main_screen.c
@@ -1,7 +1,7 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
+static TextLayer *s_title_layer;
 static GBitmap *s_steps_icon_bitmap, *s_sleep_icon_bitmap, *s_water_icon_bitmap;
 static BitmapLayer *s_steps_icon_bitmap_layer, *s_sleep_icon_bitmap_layer, *s_water_icon_bitmap_layer;
 
@@ -11,18 +11,18 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
+  s_title_layer = text_layer_create(
       GRect(0, 0, bounds.size.w, 50));
 
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "Catnap");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(s_title_layer, GColorClear);
+  text_layer_set_text_color(s_title_layer, GColorBlack);
+  text_layer_set_text(s_title_layer, "Catnap");
+  text_layer_set_font(s_title_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_title_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_title_layer));
   
   // .png pictures (bitmap): will need to be specified as bitmap resource type in appinfo.json !!!
   // https://developer.pebble.com/guides/pebble-apps/resources/image-resources/
@@ -48,7 +48,7 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_title_layer);
 }
 
 static void init() {
