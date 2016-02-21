fce2aa0611d720aa2763831112dc167c9ea4b4e5
Vivien <vchen3@wellesley.edu>
2016-02-20 19:12:36 -0500

    Working on making data appear

--- src/catNap.c
+++ src/catNap.c
@@ -1,7 +1,38 @@
+
 #include <pebble.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+
+char *itoa(int num){
+    static char buff[20] = {};
+    int i = 0, temp_num = num, length = 0;
+    char *string = buff;
+    if(num >= 0) {
+      // count how many characters in the number
+      while(temp_num) {
+        temp_num /= 10;
+        length++;
+      }
+      // assign the number to the buffer starting at the end of the 
+      // number and going to the begining since we are doing the
+      // integer to character conversion on the last number in the
+      // sequence
+      for(i = 0; i < length; i++) {
+        buff[(length-1)-i] = '0' + (num % 10);
+        num /= 10;
+      }
+      buff[i] = '\0'; // can't forget the null byte to properly end our string
+      return buff;
+    }
+    else
+    return "Unsupported Number";
+    return string;
+}
 
 static void main_window_load(Window *window) {
   // Get information about the Window
@@ -12,10 +43,31 @@ static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
+  int stepsToday = 0;
+  #if defined(PBL_HEALTH)
+
+    // Make a timestamp for now
+  time_t end = time(NULL);
+
+  // Make a timestamp for the last hour's worth of data
+  time_t start = end - SECONDS_PER_HOUR;
+
+  // Check data is available
+  HealthServiceAccessibilityMask result = 
+      health_service_metric_accessible(HealthMetricStepCount, start, end);
+  if(result & HealthServiceAccessibilityMaskAvailable) {
+    // Data is available! Read it
+    HealthValue steps = health_service_sum(HealthMetricStepCount, start, end);
+    stepsToday = (int)steps;
+  } 
+  #endif
+
+  char* totalSteps = itoa(stepsToday);
+
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "hello");
+  text_layer_set_text(s_time_layer, totalSteps);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
